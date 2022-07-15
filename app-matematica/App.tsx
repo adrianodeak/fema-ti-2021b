c3dfa0cb80673fe02fc17ba3a0f8e55d41fb8022
Adriano Deak <86475443+coderocketdev@users.noreply.github.com>
2022-07-15 20:43:32 -0300

    Criação de layout do app de matemática

---
+++ app-matematica/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+
+export default function App() {
+  const [text, onChangeText] = useState();
+
+  return (
+    <View>
+      <Header/>
+      
+      <View style={styles.container}>
+        <Input text={text} onChangeText={onChangeText}/>
+        <Button title='Calcular' onPress={() => {}}/>
+        
+        <Text style={styles.textResult}>
+          Resultado: 
+          <Text style={{paddingLeft: 8, fontWeight: '700'}}>{text}</Text>
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+function Header() {
+  return (
+    <View style={styles.headerBox}>
+      <Text style={styles.headerText}>App Matemática</Text>
+    </View>
+  );
+}
+
+function Input({text, onChangeText}) {
+  return (
+    <View style={{width: '100%'}}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={'Informe seu cálculo'}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerBox: {
+    height: 60,
+    backgroundColor: '#4867D7',
+  },
+  headerText: {
+    padding: 15,
+    fontSize: 20,
+    color: '#fff'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1
+  },
+  textResult: {
+    paddingTop: 40,
+    fontSize: 20
+  }
+});
